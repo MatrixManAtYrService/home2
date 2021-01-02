5cf8636b70b95a4815c1f40e16e0593cb378cdb6
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2021-01-01 21:23:52 -0700

    missed a few

--- .config/input/Choedan-Kal.sh
+++ .config/input/Choedan-Kal.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 touchpad=$(xinput list | grep HAILUCK | grep Touchpad | egrep -o 'id=[0-9]+' | egrep -o '[0-9]*')
-xinput --set-prop $touchpad 297 0.7
+prop=$(xinput --list-props $touchpad | grep 'Accel Speed' | head -1 | awk '{print $4}' | tr -d '():')
+xinput --set-prop $touchpad $prop 0.7
